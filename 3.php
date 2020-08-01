5b7071a836f453a28e595d30cf44fc6bc9d465dd
lies046 <skytree.skysky@gmail.com>
2020-08-01 13:25:33 +0900

    Add 3章

---
+++ 3.php
@@ -0,0 +1,15 @@
+<?php
+// $x = 100;
+// $y = ++$x;
+// print "{$x}<br>";
+// print "{$y}<br>";
+
+// $i = 100;
+// $j = $i++;
+// print "{$i}<br>";
+// print $j;
+// "<br>"
+
+// $add = bcadd(0.1, 0.7, 1);
+// $mul = bcmul($add, 10, 1);
+// print floor($mul);
