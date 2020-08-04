d0337da4b86ab7a404532c3694de1ca081126d93
lies046 <skytree.skysky@gmail.com>
2020-08-04 13:27:52 +0900

    Add 8

---
+++ 8.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form method="POST" action="post.php">
+    <label for="name">名前:</label>
+    <input id="name" type="text" name="name" size="15">
+    <input type="submit" value="送信">
+  </form>
+</body>
+
+</html>
